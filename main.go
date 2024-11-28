d0dc0ffce3595c14c3b4a66c40b3689b2bf2f0c8
Linabousbih <linabousbih@gmail.com>
2024-11-28 14:08:21 +0100

    db connect

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const uri = "mongodb://localhost:27017"
+
+func main() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("MongoDB ping failed: %v", err)
+	}
+
+	log.Println("Successfully connected to MongoDB!")
+}
